a132451e57f1b3eb94f18ba4e7137404e2c23974
Anurag chakravarty <anuragqwerty09@outlook.com>
2026-05-19 20:13:37 +0530

    feat: Adding swagger for api documentation

--- src/Features/User/dto/user.dto.ts
+++ src/Features/User/dto/user.dto.ts
@@ -1,16 +1,22 @@
 // They map the request objects.
 import { IsString,IsEmail,IsNumber,IsNotEmpty,MinLength ,IsDate, IsAlphanumeric} from "class-validator"
 //need to see if the userDto needs an id parameter 
+import { ApiProperty } from "@nestjs/swagger"
+
 export class UserDto{
 
+    @ApiProperty()
 @IsString()
 @IsNotEmpty()
 readonly username: string
 
+@ApiProperty()
 @IsAlphanumeric()
 @IsNotEmpty()
 readonly password:string
 
+
+@ApiProperty()
 @IsEmail()
 readonly email:string
 
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config= new DocumentBuilder()
+  .setTitle("server_api")
+  .setDescription("All the api's belong to the game server backend")
+  .setVersion("1.0")
+  .addTag("game")
+  .build()
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
     forbidNonWhitelisted:true,
     transform:true,
   }))
+  const documetFactory=()=> SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api',app,documetFactory)
+    
+  
   await app.listen(process.env.PORT ?? 3004);
 }
 bootstrap();
